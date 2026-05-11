8a642cb1eb2cd03434a5c4f8a2c10c64cfe519a2
zuhdiali <zuhdialihisyam@gmail.com>
2026-05-11 09:39:04 +0700

    fix bug estimasi honor mitra

--- app/Http/Controllers/MitraController.php
+++ app/Http/Controllers/MitraController.php
@@ -123,10 +123,10 @@ class MitraController extends Controller
 
         $kegiatan_mitra_awal = KegiatanLampiran::where('peserta_id', $id)->where('tipe_personil', 'mitra')->get();
         $kegiatan_mitra = [];
-        dd($kegiatan_mitra_awal);
+        // dd($kegiatan_mitra_awal);
         foreach ($kegiatan_mitra_awal as $km) {
             $kegiatan = Kegiatan::find($km->kegiatan_id);
-            if (!$kegiatan->tgl_mulai) {
+            if (!$kegiatan) {
                 continue;
             }
             if (Carbon::parse($kegiatan->tgl_mulai)->month == $bulan && Carbon::parse($kegiatan->tgl_mulai)->year == $tahun) {
